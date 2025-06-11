92d50ae73fe1c41cd7db6f2a852809c95f71f5c6
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2025-06-11 12:26:07 +0300

    feat: add components

--- src/install.js
+++ src/install.js
@@ -1,9 +1,14 @@
-import generateCSSFile from "./generateCSSFile.js";
-import generateTWFile from "./generateTWFile.js";
+import generateCSSFile from "./scripts/generateCSSFile.js";
+import generateTWFile from "./scripts/generateTWFile.js";
 
 const install = async () => {
+  try {
+
   await generateCSSFile();
   await generateTWFile();
+  } catch (error) {
+    console.error("Error in install script:", error);
+  }
 };
 
 install();
