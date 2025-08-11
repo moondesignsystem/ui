523dbb93f5680f72a98ab409719bb514de45dc6f
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2025-08-11 16:10:56 +0300

    feat: add createHTMLComponent helper

---
+++ docs/stories/components/Snackbar/Snackbar.tsx
@@ -0,0 +1,34 @@
+import { createHTMLComponent } from "../../shared/utils/createHTMLComponent";
+import joinClassnames from "../../shared/utils/joinClassnames";
+import starIcon from "../../shared/icons/starIcon";
+import getClasses from "./utils/getClasses";
+
+export const VARIANTS = ["neutral", "positive", "caution", "info"] as const;
+
+export type Props = {
+  title: string;
+  hasStartIcon: boolean;
+  hasActionButton: boolean;
+  actionLabel: string;
+  variant: (typeof VARIANTS)[number];
+};
+
+export const Snackbar = ({
+  title,
+  hasStartIcon,
+  hasActionButton,
+  actionLabel,
+  variant,
+}: Props) => (
+  <div className={joinClassnames(["moon-snackbar", getClasses(variant)])}>
+    {hasStartIcon && <span dangerouslySetInnerHTML={{ __html: starIcon }} />}
+    <span className="moon-snackbar-title">{title}</span>
+    {hasActionButton && (
+      <button className="moon-snackbar-action">{actionLabel}</button>
+    )}
+  </div>
+);
+
+const createSnackbar = createHTMLComponent(Snackbar);
+
+export default createSnackbar;
