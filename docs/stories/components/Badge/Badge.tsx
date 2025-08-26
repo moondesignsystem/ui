70336c97b4eae817a389ba81abe55e7272a7467b
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2025-08-26 16:42:28 +0300

    feat: add context to Badge, Tag

--- docs/stories/components/Badge/Badge.tsx
+++ docs/stories/components/Badge/Badge.tsx
@@ -1,11 +1,26 @@
 import { createHTMLComponent } from "../../shared/utils/createHTMLComponent";
+import CONTEXTS from "../../shared/contexts";
+import joinClassnames from "../../shared/utils/joinClassnames";
+import getClasses from "./utils/getClasses";
+
+export const VARIANTS = ["fill", "soft", "outline"] as const;
 
 export type Props = {
   children: React.ReactNode;
+  variant: (typeof VARIANTS)[number];
+  context: (typeof CONTEXTS)[number];
 };
 
-export const Badge = ({ children }: Props) => (
-  <div className="moon-badge">{children}</div>
+export const Badge = ({ children, variant, context }: Props) => (
+  <div
+    className={joinClassnames([
+      "moon-badge",
+      getClasses(variant),
+      getClasses(context),
+    ])}
+  >
+    {children}
+  </div>
 );
 
 const createBadge = createHTMLComponent(Badge);
